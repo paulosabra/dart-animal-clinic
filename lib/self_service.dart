0e1c0d125746b2a1a217d4f5e1434f5c0a69a3a9
Paulo Sabra <paulosabra.dev@gmail.com>
2024-08-22 19:09:28 -0300

    feat: 2nd delivery

--- lib/self_service.dart
+++ lib/self_service.dart
@@ -1,4 +1,11 @@
 void selfService() {
+  String address =
+      'Rua Iabiçu-Guaçu, 2024 - Vila Rio Branco, São Paulo - SP, 03875-010';
+  String phone = '(11) 92820-6866';
+
   print(
-      'Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento');
+      'Sejam bem-vindos a loja Cuidapet! Em breve teremos um sistema de autoatendimento\n');
+
+  print(address);
+  print(phone);
 }
